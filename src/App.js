1e8a0aeeb1f318508fb05716c8d7766ad6a4d446
AtulShince27 <atulk.shince@gmail.com>
2023-08-24 22:38:25 +0530

    Update 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,46 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="score-card">
+        <div className="result-column">
+          <h3>Your Result</h3>
+          <div className="result-box">
+            <p><span id='total-score'>76</span> of 100</p>
+          </div>
+          <h2>Great.</h2>
+          <p> You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
+        <div className="test-scores-column">
+          <h2>Summary</h2>
+          <div className='test-score-row' id="reaction-test-score">
+            <div className='icons' id='reaction-icon'> </div>
+            <span id='reaction-header'>Reaction </span>
+            <span className='individual-score'>80 / 100</span>
+          </div>
+          <div className='test-score-row' id="memory-test-score">
+            <div className='icons' id='memory-icon'> </div>
+            <span id='memory-header'>Memory</span>
+            <span className='individual-score'>92 / 100</span>
+          </div>
+          <div className='test-score-row' id="verbal-test-score">
+            <div className='icons' id='verbal-icon'></div>
+            <span id='verbal-header'>Verbal</span> 
+            <span className='individual-score'>61 / 100</span>
+          </div>
+          <div className='test-score-row' id="visual-test-score">
+            <div className='icons' id='visual-icon'> </div>
+            <span id='visual-header'>Visual</span>
+            <span className='individual-score'>72 / 100</span>
+          </div>
+          <div id="continue-button">
+            Continue
+          </div>
+        </div>
+      </div>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Atul Shince</a>.
+      </div>
     </div>
   );
 }
